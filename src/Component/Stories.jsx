0d31b00e7da6e14ae18245a34a0c88bbc3a817cf
AJAY-CHOUDHARY786 <ak8032161@gmail.com>
2023-10-31 12:56:03 +0530

    new-update

--- src/Component/Stories.jsx
+++ src/Component/Stories.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+
 import "../Style/stories.css";
 import Testimonial from "./Testimonial";
+import React, { useRef } from 'react';
+import myVideo from "../Component/Images/VN20231019_174742.mp4";
 const Stories = () => {
+  const videoRef = useRef(null);
+  const videoStyle = {
+    width: "100%",
+    padding: "20px",
+  };
+
+  const toggleFullScreen = () => {
+    if (videoRef.current) {
+      if (videoRef.current.requestFullscreen) {
+        videoRef.current.requestFullscreen();
+      } else if (videoRef.current.mozRequestFullScreen) {
+        videoRef.current.mozRequestFullScreen();
+      } else if (videoRef.current.webkitRequestFullscreen) {
+        videoRef.current.webkitRequestFullscreen();
+      } else if (videoRef.current.msRequestFullscreen) {
+        videoRef.current.msRequestFullscreen();
+      }
+    }
+    };
   return (
     <>
       <section>
@@ -95,6 +116,58 @@ const Stories = () => {
             </div>
           </div>
         </div>
+
+
+
+
+
+
+        <div className="vid-container">
+          <div className="vid-main">
+            <div className="stories-heading">
+              <h1>Our Video</h1>
+            </div>
+            <div className="vid-section">
+              <div className="vid-1">
+                <video  ref={videoRef} controls={false} style={videoStyle} onClick={toggleFullScreen}>
+                  <source src={myVideo} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+
+              <div className="vid-1">
+                <video ref={videoRef} controls={false}  style={videoStyle} onClick={toggleFullScreen}>
+                  <source src={myVideo} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+              <div className="vid-1">
+                <video  ref={videoRef} controls={false}  style={videoStyle} onClick={toggleFullScreen}>
+                  <source src={myVideo} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+              <div className="vid-1">
+                <video  ref={videoRef} controls={false} style={videoStyle} onClick={toggleFullScreen}>
+                  <source src={myVideo} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+              <div className="vid-1">
+                <video   ref={videoRef} controls={false} style={videoStyle} onClick={toggleFullScreen}>
+                  <source src={myVideo} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+              <div className="vid-1">
+                <video ref={videoRef} controls={false}   style={videoStyle} onClick={toggleFullScreen} >
+                  <source src={myVideo} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
       <section>
         <div className="feature-container">
@@ -104,21 +177,40 @@ const Stories = () => {
                 <h1>Feature And Appriciations</h1>
               </div>
               <div className="img-cont">
-               <div className="stor-img"><img style={{ width: "100%" }} src="https://shaandaarevents.com/wp-content/uploads/2022/06/wedding.gif" alt="" /></div>
-               <div className="stor-img"><img style={{ width: "100%" }} src="https://shaandaarevents.com/wp-content/uploads/2022/06/wedding.gif" alt="" /></div>
-               <div className="stor-img"><img style={{ width: "100%" }} src="https://shaandaarevents.com/wp-content/uploads/2022/06/wedding.gif" alt="" /></div>
-               <div className="stor-img"><img style={{ width: "100%" }} src="https://shaandaarevents.com/wp-content/uploads/2022/06/wedding.gif" alt="" /></div>
-
-             
-             </div>
+                <div className="stor-img">
+                  <img
+                    style={{ width: "100%" }}
+                    src="https://shaandaarevents.com/wp-content/uploads/2022/06/wedding.gif"
+                    alt=""
+                  />
+                </div>
+                <div className="stor-img">
+                  <img
+                    style={{ width: "100%" }}
+                    src="https://shaandaarevents.com/wp-content/uploads/2022/06/wedding.gif"
+                    alt=""
+                  />
+                </div>
+                <div className="stor-img">
+                  <img
+                    style={{ width: "100%" }}
+                    src="https://shaandaarevents.com/wp-content/uploads/2022/06/wedding.gif"
+                    alt=""
+                  />
+                </div>
+                <div className="stor-img">
+                  <img
+                    style={{ width: "100%" }}
+                    src="https://shaandaarevents.com/wp-content/uploads/2022/06/wedding.gif"
+                    alt=""
+                  />
+                </div>
+              </div>
             </div>
-
-           
-
           </div>
         </div>
       </section>
-      <Testimonial/>
+      <Testimonial />
     </>
   );
 };
--- src/Pages/Testimoniol.jsx
+++ src/Pages/Testimoniol.jsx
@@ -31,50 +31,6 @@ const Testimoniol = () => {
               </div>
             </div>
           </div>
-          {/* <div className="testimoniol-inner">
-            <div className="testimoniol-img-box">
-              <img src={testimg} style={{ width: "100%" }} alt="" />
-              <h3> Parul & Obiroy </h3>
-
-              <div className="testimoniol-para">
-                <h1> Paurl & Oberoi </h1>
-                <p>
-                  {" "}
-                  Shaandaar events , the best wedding planner was the best
-                  decision I ever made for my big fat Indian wedding. The
-                  intricacies and details they worked upon were commendable.
-                  They suggested boho themed mehendi and The way they executed
-                  it made my event a viral trend. Even during our wedding they
-                  managed to light up the entire property with pyros and it made
-                  my special moment even more dreamy. I am glad I chose them for
-                  my wedding and they proved themselves as the best wedding
-                  planner.{" "}
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="testimoniol-inner">
-            <div className="testimoniol-img-box">
-              <img src={testimg} style={{ width: "100%" }} alt="" />
-              <h3> Parul & Obiroy </h3>
-
-              <div className="testimoniol-para">
-                <h1> Paurl & Oberoi </h1>
-                <p>
-                  {" "}
-                  Shaandaar events , the best wedding planner was the best
-                  decision I ever made for my big fat Indian wedding. The
-                  intricacies and details they worked upon were commendable.
-                  They suggested boho themed mehendi and The way they executed
-                  it made my event a viral trend. Even during our wedding they
-                  managed to light up the entire property with pyros and it made
-                  my special moment even more dreamy. I am glad I chose them for
-                  my wedding and they proved themselves as the best wedding
-                  planner.{" "}
-                </p>
-              </div>
-            </div>
-          </div> */}
         </div>
       </section>
     </>
